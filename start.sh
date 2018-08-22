70451937c671647ee7429aa828959cf263d8289b
Alexander Malic <alexander.malic@gmail.com>
2018-08-22 23:01:59 +0200

    completed switch to docker-compose

--- start.sh
+++ start.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
-ELK_VERSION="5.6.10"
 NGINX_VERSION="1.15.2"
 
-# 1. First let's start Elasticsearch
+# 1. First let's start our ELK-Stack
+docker-compose up -d
 
-docker run -d --rm --name elasticsearch \
-  elasticsearch:$ELK_VERSION
+# this could be exported to a environment variable
+LOGSTASH_ADDRESS=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nginxelk_logstash_1)
 
-# 2. Now we can start logstash (host from config has to match link)
-
-docker build -t mylogstash ./logstash
-
-docker run -d --rm --name logstash \
-  --link elasticsearch:elasticsearch \
-  mylogstash
-
-# 3. Let's run Kibana
-# If you want to check it out go to http://localhost:5601
-# Don't forget to create an Index pattern
-
-docker run -d --rm --name kibana \
-  --link elasticsearch:elasticsearch \
-  -p 5601:5601 \
-  kibana:$ELK_VERSION
-
-# 4. Finally let's start our webserver
-# Note that logging can not be connected via Link
-# --> this will not work in a Docker-Compose File
-# Except $LOGSTASH_ADDRESS is defined as environment variable
-
-LOGSTASH_ADDRESS=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' logstash)
-
+# 2. Now let's start a test application
+# 2.1 try to query http://localhost and some wrong url's
+# 2.2 set up an index on http://localhost:5601
 docker run -d --rm --name nginx \
   --log-driver=gelf \
   --log-opt gelf-address=udp://$LOGSTASH_ADDRESS:12201 \
